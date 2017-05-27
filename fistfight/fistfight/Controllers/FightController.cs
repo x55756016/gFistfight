6a1e03b9c148c541c5d9285a029a812fd42352b6
x55756016 <55756016@qq.com>
2017-05-27 17:10:27 +0800

    page

---
+++ fistfight/fistfight/Controllers/FightController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fistfight.Controllers
+{
+    public class FightController : Controller
+    {
+        //
+        // GET: /Fight/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
